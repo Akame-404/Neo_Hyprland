ece46219c105e1c03a568dcf6a045c8bac3920b8
Akame-404 <neoxique@gmail.com>
2024-06-20 18:24:29 -0400

    é

--- restore.sh
+++ restore.sh
@@ -4,3 +4,6 @@ scrDir=$(dirname "$(realpath "$0")")
 
 cp -rf $scrDir/.config/* ~/.config 
 cp -rf $scrDir/.zshrc ~/
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+sudo cp -rf $scrDir/Sources/grub .config 
+sudo cp -rf $scrDir/Sources/arcs/Neo_Grub /usr/share/grub/themes/
